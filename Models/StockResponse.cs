f919abd8778d5a441a2111cdfcf630496d19c652
jossai87 <jossai6@gatech.edu>
2023-04-16 09:00:15 -0500

    updated API to return more values

--- Models/StockResponse.cs
+++ Models/StockResponse.cs
@@ -4,23 +4,29 @@ namespace StockApp.Models
 {
     public class StockResponse
     {
-        [JsonPropertyName("data")]
-        public StockData[] theData { get; set; }
+        [JsonPropertyName("meta")]
+        public MetaData theMetaData { get; set; }
+
+        [JsonPropertyName("values")]
+        public StockValue[] theValues { get; set; }
 
         [JsonPropertyName("status")]
         public string theStatus { get; set; }
     }
 
-    public class StockData
+    public class MetaData
     {
         [JsonPropertyName("symbol")]
         public string theSymbol { get; set; }
 
-        [JsonPropertyName("name")]
-        public string theName { get; set; }
+        [JsonPropertyName("interval")]
+        public string theInterval { get; set; }
 
         [JsonPropertyName("currency")]
         public string theCurrency { get; set; }
+
+        [JsonPropertyName("exchange_timezone")]
+        public string theExchangeTimezone { get; set; }
 
         [JsonPropertyName("exchange")]
         public string theExchange { get; set; }
@@ -28,10 +34,28 @@ namespace StockApp.Models
         [JsonPropertyName("mic_code")]
         public string theMicCode { get; set; }
 
-        [JsonPropertyName("country")]
-        public string theCountry { get; set; }
-
         [JsonPropertyName("type")]
         public string theType { get; set; }
+    }
+
+    public class StockValue
+    {
+        [JsonPropertyName("datetime")]
+        public string theDateTime { get; set; }
+
+        [JsonPropertyName("open")]
+        public string theOpen { get; set; }
+
+        [JsonPropertyName("high")]
+        public string theHigh { get; set; }
+
+        [JsonPropertyName("low")]
+        public string theLow { get; set; }
+
+        [JsonPropertyName("close")]
+        public string theClose { get; set; }
+
+        [JsonPropertyName("volume")]
+        public string theVolume { get; set; }
     }
 }
